58b9cae13556c272df6725d050e572dc03d62ffb
caitlinjee <caitlinjeespn@gmail.com>
2021-01-08 14:52:19 +0800

    Setup PUN and Mapbox

---
+++ Assets/Scripts/LobbyTopPanel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+namespace Com.NucleAr.MapAr
+{
+    class LobbyTopPanel : MonoBehaviour
+    {
+        private readonly string connectionStatusMessage = "    Connection Status: ";
+        [Header("UI References")]
+        public Text ConnectionStatusText;
+        public void Update()
+        {
+            ConnectionStatusText.text = connectionStatusMessage + PhotonNetwork.NetworkClientState;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerListEntry.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using Photon.Pun.UtilityScripts;
+
+namespace Com.NucleAr.MapAr
+{
+    class PlayerListEntry : MonoBehaviour
+    {
+        [Header("UI References")]
+        public Text PlayerNameText;
+
+        public Image PlayerColorImage;
+        public Button PlayerReadyButon;
+        public Image PlayerReadyImage;
+
+        private int ownerId;
+        private bool isPlayerReady;
+
+        public void OnEnable()
+        {
+            PlayerNumbering.OnPlayerNumberingChanged += OnPlayerNumberingChanged;
+        }
+        public void Start()
+        {
+            if (PhotonNetwork.LocalPlayer.ActorNumber != ownerId)
+            {
+                PlayerReadyButon.gameObject.SetActive(false);
+            }
+            else
+            {
+                Hashtable initialProps = new Hashtable() { { NucleArManager.PLAYER_READY, isPlayerReady }, { NucleArManager.PLAYER_LIVES, NucleArManager.PLAYER_MAX_LIVES } };
+                PhotonNetwork.LocalPlayer.SetCustomProperties(initialProps);
+                PhotonNetwork.LocalPlayer.SetScore(0);
+
+                PlayerReadyButon.onClick.AddListener(() =>
+                {
+                    isPlayerReady = !isPlayerReady;
+                    SetPlayerReady(isPlayerReady);
+
+                    Hashtable props = new Hashtable() { { NucleArManager.PLAYER_READY, isPlayerReady } };
+                    PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+                    if (PhotonNetwork.IsMasterClient)
+                    {
+                        FindObjectOfType<LobbyMainPanel>().LocalPlayerPropertiesUpdated();
+                    }
+                });
+            }
+        }
+        public void OnDisable()
+        {
+            PlayerNumbering.OnPlayerNumberingChanged -= OnPlayerNumberingChanged;
+        }
+        public void Initialize(int playerId, string playerName)
+        {
+            ownerId = playerId;
+            PlayerNameText.text = playerName;
+        }
+        private void OnPlayerNumberingChanged()
+        {
+            foreach (Player p in PhotonNetwork.PlayerList)
+            {
+                if (p.ActorNumber == ownerId)
+                {
+                    PlayerColorImage.color = NucleArManager.GetColor(p.GetPlayerNumber());
+                }
+            }
+        }
+        public void SetPlayerReady(bool playerReady)
+        {
+            PlayerReadyButon.GetComponentInChildren<Text>().text = playerReady ? "Ready!" : "Ready?";
+            PlayerReadyImage.enabled = playerReady;
+        }
+    }
+}
